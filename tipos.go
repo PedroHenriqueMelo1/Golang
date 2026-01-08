7394e0de70a1932e09b0ed04485921715bf999dc
PedroHenriqueMelo1 <you@example.comew>
2026-01-08 14:14:22 -0300

    name variables

---
+++ tipos.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	 Console "fmt"
+)
+
+func main() {
+ //  Console.Printf("Type: %T - Value %v\n", true, true) // % define verbos
+ //   Console.Printf("Type: %T - Value %v\n", "pedro", "cairo rocha") // Expressão %T %v, expressão com dois verbos.	
+  // Console.Printf("Type: %T - Value %v\n", 1.23, 1.23) // número que tem casas decimais são floats64
+
+}
---
+++ varconst.go
@@ -0,0 +1,14 @@
+package main 
+
+import "fmt"
+
+
+func main() {
+	var nome string // declaração de variavel var + varname + type
+	nome = "bento" 
+	fmt.Println(nome)
+
+	nome = "steph"
+
+	fmt.Println(nome)
+}
